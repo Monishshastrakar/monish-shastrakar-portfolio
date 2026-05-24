c02d9d03690ccc4b19ff1542503b479d6405efbc
Monish Shastrakar <monish@example.com>
2026-05-25 02:29:22 +0530

    chore: update vite base path for custom domain configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/monish-shastrakar-portfolio/',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
